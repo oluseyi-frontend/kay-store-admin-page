3bd7d6f45381414d6f1fac4c60660a08543fe359
kay1234567 <alawiyeolukayode@gmail.com>
2020-08-19 04:42:04 +0100

    updates

--- src/admin-components/firebase.js
+++ src/admin-components/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDCQ9LWkbiZ2A7TOe6udFv1DN2EDN4iYEc",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "first-e-commerce-71d37.firebaseapp.com",
   databaseURL: "https://first-e-commerce-71d37.firebaseio.com",
   projectId: "first-e-commerce-71d37",
   storageBucket: "first-e-commerce-71d37.appspot.com",
   messagingSenderId: "867574054697",
-  appId: "1:867574054697:web:960507c55a85aa554b8138",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: "G-BE07CMHELZ",
 };
 // Initialize Firebase
